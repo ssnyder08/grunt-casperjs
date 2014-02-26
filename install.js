754ba9a60ea7bfa1cf6a2aa37d830664a7623408
Scott Tustison <scott.a.tustison@lmco.com>
2014-02-26 12:49:42 -0700

    Modified the install.js script so that it was capable of getting around an HTTP proxy
    Added casperjs to package.json so that npm could take care of downloading casper rather than doing it via the install.js script. The script should only be used if that fails for some reason, though I'm not sure why it would fail.

--- install.js
+++ install.js
@@ -1,20 +1,23 @@
 'use strict'
 
-var cp = require('child_process')
-var fs = require('fs')
-var http = require('http')
-var https = require('https')
-var path = require('path')
-var url = require('url')
-var rimraf = require('rimraf').sync
-var AdmZip = require('adm-zip')
+var cp = require('child_process');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
+var path = require('path');
+var url = require('url');
+var rimraf = require('rimraf').sync;
+var AdmZip = require('adm-zip');
+var npmconf = require('npmconf');
+var kew = require('kew');
+var tunnel = require('tunnel');
 
 fs.existsSync = fs.existsSync || path.existsSync
 
-var libPath = path.join(__dirname, 'lib', 'casperjs')
-var tmpPath = path.join(__dirname, 'tmp')
-var version = '1.0.3'
-var downloadUrl = 'https://codeload.github.com/n1k0/casperjs/zip/' + version
+var libPath = path.join(__dirname, 'lib', 'casperjs');
+var tmpPath = path.join(__dirname, 'tmp');
+var version = '1.1-beta3';
+var downloadUrl = 'https://github.com/n1k0/casperjs/archive/' + version + '.zip';
 
 
 
@@ -37,47 +40,106 @@ function isCasperInstalled(notInstalledCallback) {
 }
 
 function tidyUp() {
+    console.log("Tidying up.");
     rimraf(tmpPath);
+    console.log("Finished tidy.");
 }
 
 function unzipTheZippedFile() {
+    console.log('Attemping to unzip the file.');
     var zip = new AdmZip(path.join(tmpPath, 'archive.zip'));
     zip.extractAllTo(libPath, true);
-
+    console.log('Extracted contents.');
     if (process.platform != 'win32') {
+        console.log('Creating symlinks.');
         var pathToCommand = path.join(libPath, 'casperjs-' + version, 'bin', 'casperjs');
         fs.symlinkSync(pathToCommand, './casperjs');
+        console.log('Created symlink and statting file.');
         var stat = fs.statSync(pathToCommand);
         if (!(stat.mode & 64)) {
-            fs.chmodSync(pathToCommand, '755')
+            console.log('Performing chmod on file.');
+            fs.chmodSync(pathToCommand, '755');
+            console.log('Finished chmod on file.');
         }
     }
+    console.log('Calling tidy.');
     tidyUp();
 }
 
 function downloadZipFromGithub() {
+    console.log('Attemping to download the zip from github');
     var file = fs.createWriteStream(path.join(tmpPath, "archive.zip"));
     var lengthSoFar = 0;
-    var request = https.get(downloadUrl, function(response) {
-        if (response.statusCode === 301 || response.statusCode === 302) {
-            downloadUrl = response.headers.location;
-            downloadZipFromGithub();
-        } else {
-            response.pipe(file);
-            response.on('data', function(chunk) {
-                console.log('Receiving ' + Math.floor((lengthSoFar += chunk.length) / 1024) + 'K...' );
-            }).
-                on('end', unzipTheZippedFile).
-                on('error', function(e) {
-                    console.log('An error occured whilst trying to download Casper.JS ' + e.message);
-                    tidyUp();
-                });
+    var npmconfDeferred = kew.defer()
+    npmconf.load(npmconfDeferred.makeNodeResolver())
+    npmconfDeferred.promise.then(function (conf) {
+        console.log('Got configuration');
+        var request = https.get(getProxyOptions(conf.get('proxy')), function(response) {
+                console.log('Performing http get');
+                if (response.statusCode === 301 || response.statusCode === 302) {
+                    console.log("Received an error response code: "+response.statusCode);
+                    downloadUrl = response.headers.location;
+                    downloadZipFromGithub();
+                } else {
+                    response.pipe(file);
+                    response.on('data', function(chunk) {
+                        console.log('Receiving ' + Math.floor((lengthSoFar += chunk.length) / 1024) + 'K...' );
+                    }).
+                        on('end', unzipTheZippedFile).
+                        on('error', function(e) {
+                            console.log('An error occured whilst trying to download Casper.JS ' + e.message);
+                            tidyUp();
+                        });
+                }
+            });
+            request.on('error', function(e) {
+                console.log('An error occured whilst trying to download Casper.JS ' + e.message);
+                tidyUp();
+            });
+    });
+
+}
+
+function getProxyOptions(proxyUrl) {
+    if (proxyUrl) {
+        console.log('Found proxy url: '+proxyUrl);
+        var proxyOptions = url.parse(proxyUrl);
+        var options = url.parse(downloadUrl);
+
+        var tunnelingAgent = tunnel.httpsOverHttp({
+            rejectUnauthorized: false,
+
+            proxy: { // Proxy settings
+                host: proxyOptions.hostname, // Defaults to 'localhost'
+                port: proxyOptions.port, // Defaults to 80
+
+                // Basic authorization for proxy server if necessary
+                proxyAuth: proxyOptions.auth,
+
+                // Header fields for proxy server if necessary
+                headers: {
+                    'User-Agent': 'curl/7.21.4 (universal-apple-darwin11.0) libcurl/7.21.4 OpenSSL/0.9.8r zlib/1.2.5'
+                }
+            }
+        });
+
+        tunnelingAgent.rejectUnauthorized = false;
+        options.agent = tunnelingAgent;
+        options.path = downloadUrl;
+        options.headers = { Host: url.parse(downloadUrl).host };
+        console.log('Setting up connection to host: '+options.headers.Host);
+        console.log('Downloading from: '+options.path);
+        options.headers['User-Agent'] = 'curl/7.21.4 (universal-apple-darwin11.0) libcurl/7.21.4 OpenSSL/0.9.8r zlib/1.2.5';
+        if (options.auth) {
+            options.headers['Proxy-Authorization'] = 'Basic ' + new Buffer(options.auth).toString('base64');
+            delete options.auth;
         }
-    });
-    request.on('error', function(e) {
-        console.log('An error occured whilst trying to download Casper.JS ' + e.message);
-        tidyUp();
-    });
+
+        console.log('Returning options');
+        return options;
+    } else {
+        return url.parse(downloadUrl);
+    }
 }
 
 isCasperInstalled(function() {
